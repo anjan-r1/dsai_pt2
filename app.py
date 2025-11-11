af5d3eba7cc721349f82209e4ac1e6476c44135b
anjan-r1 <anjankumar.tcs@gmail.com>
2025-11-11 13:02:19 +0000

    Add Flask app and templates

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    q = float(request.form.get("q"))
+    return(render_template("prediction.html", r=(-50.6*q)+90.2))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run()
