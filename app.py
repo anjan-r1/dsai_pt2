9d05d17a0b5bfc80a1e5bd37cf70e412cd987086
anjan-r1 <anjankumar.tcs@gmail.com>
2025-11-12 13:32:50 +0000

    your commit message

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 if os.path.exists('.env'):
     load_dotenv()
 
-os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
 client = Groq()
 
 app = Flask(__name__)
